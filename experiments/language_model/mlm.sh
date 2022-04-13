791983f4ef04f059c99141f1a3589e1ec9176863
Rahul Huilgol <rahulhuilgol@gmail.com>
2022-04-13 13:13:23 -0700

    Update mlm.sh

--- experiments/language_model/mlm.sh
+++ experiments/language_model/mlm.sh
@@ -82,8 +82,7 @@ case ${init,,} in
 		;;
 esac
 
-python -m DeBERTa.apps.run \
-	--model_config config.json  \
+python -m DeBERTa.apps.run --model_config config.json  \
 	--tag $tag \
 	--do_train \
 	--use_mpi 1 \
